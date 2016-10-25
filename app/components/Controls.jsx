ece3324c15ad2a1da626023cf572ea8246c21e7d
Daniel Charman <hello@danielcharman.com>
2016-10-25 21:53:58 +1100

    Started timer component

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -17,7 +17,7 @@ let Controls = React.createClass({
           return (
             <button className="button secondary" onClick={this._onStatusChange('paused')}>Pause</button>
           );
-        }else if(countdownStatus == 'paused') {
+        }else {
           return (
             <button className="button primary" onClick={this._onStatusChange('started')}>Start</button>
           );
--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,12 +1,33 @@
 let React = require('react');
 
+let Clock = require('Clock');
+let Controls = require('Controls');
 
 let Timer = React.createClass({
+  getInitialState: function() {
+    return {
+      count: 0,
+      timerStatus: 'stopped'
+    }
+  },
+  _handleSetCountdown: function(seconds) {
+    this.setState({
+      count: seconds,
+      timerStatus: 'started'
+    });
+  },
+  _handleStatusChange: function(newStatus) {
+    this.setState({
+      timerStatus: newStatus
+    });
+  },
   render: function() {
+    let {count, timerStatus} = this.state;
     return (
       <div>
-        Timer
-
+        <h1 className="page-title">Timer App</h1>
+        <Clock totalSeconds={count}/>
+        <Controls countdownStatus={timerStatus} onStatusChange={this._handleStatusChange}/>
       </div>
     );
   }
