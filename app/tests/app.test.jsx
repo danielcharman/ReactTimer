322e6edc823439667f6950c5217b83f3a2fa082a
Daniel Charman <hello@danielcharman.com>
2016-10-28 19:05:55 +1100

    Updated test boilerplate

--- app/tests/app.test.jsx
+++ app/tests/app.test.jsx
@@ -1,13 +1,13 @@
-let React = require('react');
-let ReactDOM = require('react-dom');
-let expect = require('expect');
-let $ = require('jQuery');
-let TestUtils = require('react-addons-test-utils');
-
-let App = require('App');
-
-describe('App', () => {
-  it('should exist', () => {
-    expect(App).toExist();
-  });
-});
+// let React = require('react');
+// let ReactDOM = require('react-dom');
+// let expect = require('expect');
+// let $ = require('jQuery');
+// let TestUtils = require('react-addons-test-utils');
+//
+// let App = require('App');
+//
+// describe('App', () => {
+//   it('should exist', () => {
+//     expect(App).toExist();
+//   });
+// });
